62c2e91470a0ba1bec8bb881ddaa3138aaec2e26
Daniel Emehinola <dan.emehin@gmail.com>
2023-08-17 07:13:02 -0600

    updates to frontend files

---
+++ src/utils/http-request.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const getListings = async (term, location, sort) => {
+  try {
+    const response = await axios.get("http://localhost:8080/api", {
+      params: {
+        term: term,
+        location: location,
+        sort: sort,
+      },
+    });
+    if (response.data.businesses) {
+      return response.data.businesses.map((business) => ({
+        id: business.id,
+        imageSrc: business.image_url,
+        name: business.name,
+        address1: business.location.display_address[0],
+        address2: business.location.display_address[1],
+        latitude: business.coordinates.latitude,
+        category: business.categories[0].title,
+        rating: business.rating,
+        reviewCount: business.review_count,
+      }));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+export default getListings;
